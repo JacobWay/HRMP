67e22841c97cdbd40d2a8b9c64783f238023db0c
JacobWay <soarinblue@gmail.com>
2017-01-26 12:25:43 +0800

    add clear all checkbox functionality

--- src/components/TitleRow.js
+++ src/components/TitleRow.js
@@ -1,9 +1,17 @@
 import React, {Component} from "react";
+import $ from "jquery";
 
 class TitleRow extends Component{
+    handleClick = () => {
+        $("input").prop("checked", false);
+    }
+
     render(){
         return(
-            <h2>招聘职位<span>清空</span></h2>
+            <h2>
+                招聘职位
+                <button onClick={this.handleClick}>清空</button>
+            </h2>
         );
     }
 }
